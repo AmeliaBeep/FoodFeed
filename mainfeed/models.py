ba4c44ee164b43976922e8a1b96313ac23ef8b53
Amelia James <ameliafjjames@gmail.com>
2025-10-24 13:00:58 +0100

    Add comments to posts

--- mainfeed/models.py
+++ mainfeed/models.py
@@ -11,8 +11,20 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     #updated_on = models.DateTimeField(auto_now=True)
 
-    # class Meta:
-    #     ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
     
     def __str__(self):
         return f"Post written by {self.author}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}" 
