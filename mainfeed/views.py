2c3329decba1e5a5cd50a0726df3734cb649d39f
AmeliaBeep <ameliafjjames@gmail.com>
2025-10-28 20:48:27 +0000

    Add docstrings for view functions

--- mainfeed/views.py
+++ mainfeed/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import PostForm, PostTextForm, CommentForm
 
-from cloudinary.uploader import upload_image
 # Create your views here.
 
 class PostList(generic.ListView):
@@ -13,14 +12,29 @@ class PostList(generic.ListView):
     template_name = "mainfeed/index.html"
     paginate_by = 10
 
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# # Create your views here.
-# def index(request):
-#     return HttpResponse("Hello, World!")
-
+# TODO: handle empty User
 def create_post(request):
+    """
+    Handles POST and GET requests related to post creation.
 
+    For POST requests, a new post is added to the database:
+        - The post has its image and text fields set according to the form 
+        contents included in the request. 
+        - The user who submitted the form is set as the author.
+
+    For GET requests, render the webpage for creating posts.
+
+    Args:
+        request (HttpRequest): The request to process post creation or to 
+        serve the corresponding webpage
+
+    Returns:
+        Union[HttpRequest, HttpResponse]:
+            - Upon handling a POST request, a redirect request including a 
+            success message.
+            - Upon handling a GET request, a response containing the page 
+            and post create form to render.
+    """
     if request.method == "POST":
         post_form = PostForm(request.POST, request.FILES)
         if post_form.is_valid():
@@ -37,6 +51,7 @@ def create_post(request):
                 request, messages.ERROR,
                 'Post failed to submit'
             )
+            return HttpResponseRedirect(reverse('feed'))
     else:
         post_form = PostForm()
         return render(
@@ -47,9 +62,113 @@ def create_post(request):
             }
         )
 
+# TODO: handle empty or unauthorised User
+def edit_post(request, post_id):
+    """
+    Handles POST and GET requests related to post editing.
 
+    For POST requests, the target post in the database is updated:
+        - The post has its text field set according to the form contents 
+        included in the request.
+
+    For GET requests, render the webpage for editing a target post.
+
+    Args:
+        request (HttpRequest): The request to process post editing or to 
+        serve the corresponding webpage
+        post_id (int): The id of the post to edit.
+        
+
+    Returns:
+        Union[HttpRequest, HttpResponse]:
+            - Upon handling a POST request, a redirect request including a 
+            success message.
+            - Upon handling a GET request, a response containing the page 
+            and the post edit form to render.
+    """
+    if request.method == "POST":
+        post = get_object_or_404(Post, pk=post_id)
+        post_form = PostTextForm(request.POST, instance=post)
+
+        if post_form.is_valid() and post.author == request.user:
+            post = post_form.save(commit=True)
+            messages.add_message(request, messages.SUCCESS, 'Post Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating post!')
+        return HttpResponseRedirect(reverse('feed'))
+
+    else:    
+        post = get_object_or_404(Post, pk=post_id)
+
+        post_text_form = PostTextForm()
+        return render(
+            request,
+            "mainfeed/edit_post.html",
+            {
+                "post": post,
+                "post_text_form": post_text_form,
+            },
+        )
+
+# TODO: handle empty or unauthorised User
+def delete_post(request, post_id):
+    """
+    Handles a request to delete a post.
+
+    Args:
+        request (HttpRequest): The request to process the deletion.
+        post_id (int): The id of the post to delete.
+
+    Returns:
+        HttpResponse: a redirect request including a success message.
+    """
+    post = get_object_or_404(Post, pk=post_id)
+
+    if post.author == request.user:
+        post.delete()
+        messages.add_message(request, messages.SUCCESS, 'Post Deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'Error deleting post!')
+    return HttpResponseRedirect(reverse('feed'))
+
+# def view_comment(request, post_id, comment_id):
+#     post = get_object_or_404(Post, pk=post_id)
+#     comment = get_object_or_404(Comment, pk=comment_id)
+
+#     return render(
+#             request,
+#             "mainfeed/view_comment.html",
+#             {
+#                 "post": post,
+#                 "comment": comment,
+#             },
+#         )
+
+# TODO: handle empty User
 def create_comment(request, post_id):
+    """
+    Handles POST and GET requests related to comment creation.
 
+    For POST requests, a new comment is added to the database:
+        - The comment has its body field set according to the form 
+        contents included in the request.
+        - The user who submitted the form is set as the author.
+        - The comment is linked to its corresponding post.
+
+    For GET requests, render the webpage for creating comments.
+
+    Args:
+        request (HttpRequest): The request to process comment creation or to 
+        serve the corresponding webpage
+        post_id (int): The id of the post associated with the new comment.
+
+    Returns:
+        Union[HttpRequest, HttpResponse]:
+            - Upon handling a POST request, a redirect request including a 
+            success message.
+            - Upon handling a GET request, a response containing the page 
+            and comment create form to render.
+    """
     if request.method == "POST":
             comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
@@ -81,47 +200,30 @@ def create_comment(request, post_id):
             },
         )
 
-def view_comment(request, post_id, comment_id):
-    post = get_object_or_404(Post, pk=post_id)
-    comment = get_object_or_404(Comment, pk=comment_id)
-
-    return render(
-            request,
-            "mainfeed/view_comment.html",
-            {
-                "post": post,
-                "comment": comment,
-            },
-        )
-
-def edit_post(request, post_id):
-    
-    if request.method == "POST":
-        post = get_object_or_404(Post, pk=post_id)
-        post_form = PostTextForm(request.POST, instance=post)
-
-        if post_form.is_valid() and post.author == request.user:
-            post = post_form.save(commit=True)
-            messages.add_message(request, messages.SUCCESS, 'Post Updated!')
-        else:
-            messages.add_message(request, messages.ERROR, 'Error updating post!')
-        return HttpResponseRedirect(reverse('feed'))
-
-    else:    
-        post = get_object_or_404(Post, pk=post_id)
-
-        post_text_form = PostTextForm()
-        return render(
-            request,
-            "mainfeed/edit_post.html",
-            {
-                "post": post,
-                "post_text_form": post_text_form,
-            },
-        )
-
+# TODO: handle empty or unauthorised User
 def edit_comment(request, post_id, comment_id):
-    
+    """
+    Handles POST and GET requests related to comment editing.
+
+    For POST requests, the target comment in the database is updated:
+        - The comment has its body field set according to the form 
+        contents included in the request.
+
+    For GET requests, render the webpage for editing a target comment.
+
+    Args:
+        request (HttpRequest): The request to process comment editing or to 
+        serve the corresponding webpage
+        post_id (int): The id of the post associated with the comment.
+        comment_id (int): The id of the comment to edit.
+
+    Returns:
+        Union[HttpRequest, HttpResponse]:
+            - Upon handling a POST request, a redirect request including a 
+            success message.
+            - Upon handling a GET request, a response containing the page 
+            and comment edit form to render.
+    """    
     if request.method == "POST":
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(request.POST, instance=comment)
@@ -146,44 +248,19 @@ def edit_comment(request, post_id, comment_id):
                 "comment_form": comment_form,
             },
         ) 
-# def edit_comment(request, comment_id):
-    
-#     if request.method == "POST":
-#         post = get_object_or_404(Post, pk=post_id)
-#         post_form = PostTextForm(request.POST, instance=post)
 
-#         if post_form.is_valid() and post.author == request.user:
-#             post = post_form.save(commit=True)
-#             messages.add_message(request, messages.SUCCESS, 'Post Updated!')
-#         else:
-#             messages.add_message(request, messages.ERROR, 'Error updating post!')
-#         return HttpResponseRedirect(reverse('feed'))
-
-#     else:    
-#         queryset = Post.objects.filter(pk=post_id)
-#         post = get_object_or_404(queryset)
-
-#         post_text_form = PostTextForm()
-#         return render(
-#             request,
-#             "mainfeed/edit_post.html",
-#             {
-#                 "post": post,
-#                 "post_text_form": post_text_form,
-#             },
-#         )
-
-def delete_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-
-    if post.author == request.user:
-        post.delete()
-        messages.add_message(request, messages.SUCCESS, 'Post Deleted!')
-    else:
-        messages.add_message(request, messages.ERROR, 'Error deleting post!')
-    return HttpResponseRedirect(reverse('feed'))
-
+# TODO: handle empty or unauthorised User
 def delete_comment(request, comment_id):
+    """
+    Handles a request to delete a comment.
+
+    Args:
+        request (HttpRequest): The request to process the deletion.
+        comment_id (int): The id of the comment to delete.
+
+    Returns:
+        HttpResponse: a redirect request including a success message.
+    """
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
