278bf19e64ecc90420e87f7aae6f597523334215
AmeliaBeep <ameliafjjames@gmail.com>
2025-11-03 23:02:35 +0000

    Update user model test to have setup and to delete images uploaded to cloudinary during runtime

--- userprofile/test_models.py
+++ userprofile/test_models.py
@@ -17,11 +17,8 @@ class TestProfileUser(TestCase):
     """
 
     # TODO: Mock Cloudinary response in creating test_image
-    #       Prevent PostForm to_python subroutines from uploading test_image     
-    def test_model_is_valid(self):
-        """ 
-        Tests that UserProfile accepts valid fields.
-        """
+    #       Prevent PostForm to_python subroutines from uploading test_image  
+    def setUp(self):
         cloudinary_test_image = cloudinary.api.resource("test_image")
         test_image_url = cloudinary_test_image.get('url')
         test_image = requests.get(test_image_url)
@@ -30,18 +27,26 @@ class TestProfileUser(TestCase):
         test_image_content.name = 'test_image.jpg'
 
         image = {'image': test_image_content}
+
         test_user_profile_form = UserProfileForm({'bio': 'Test bio text'}, image)
+        
+        test_user_profile = test_user_profile_form.save(commit=False)
+        self.user_profile = test_user_profile
 
         test_user_form = UserForm(data={'username': 'test_username'})
-        user = test_user_form.save()
-
-        test_user_profile = test_user_profile_form.save(commit=False)
-        test_user_profile.user = user
+        self.user = test_user_form.save()        
         
-        test_user_profile.save()
-        test_user_profile.full_clean()
 
-        uploaded_asset_id = test_user_profile_form.cleaned_data['image'].public_id
+   
+    def test_model_is_valid(self):
+        """ 
+        Tests that UserProfile accepts valid fields.
+        """
+        self.user_profile.user = self.user
+        self.user_profile.save()
+        self.user_profile.full_clean()
+
+        uploaded_asset_id = self.user_profile.image.public_id
         cloudinary.uploader.destroy(uploaded_asset_id)
 
     def test_model_has_no_user(self):
@@ -49,23 +54,10 @@ class TestProfileUser(TestCase):
         Tests that UserProfile rejects missing user.
         """
 
-        cloudinary_test_image = cloudinary.api.resource("test_image")
-        test_image_url = cloudinary_test_image.get('url')
-        test_image = requests.get(test_image_url)
-
-        test_image_content = ContentFile(test_image.content)
-        test_image_content.name = 'test_image.jpg'
-
-        image = {'image': test_image_content}
-
-        test_user_profile_form = UserProfileForm({'bio': 'Test bio text'}, image)
-
         with self.assertRaises(IntegrityError, msg="User Profile able to be saved to database, but no user provided"):
-            test_user_profile_form.save()
-
-        uploaded_asset_id = test_user_profile_form.cleaned_data['image'].public_id
+            self.user_profile.save()
+        uploaded_asset_id = self.user_profile.image.public_id
         cloudinary.uploader.destroy(uploaded_asset_id)
 
-        
 
-    
+        
