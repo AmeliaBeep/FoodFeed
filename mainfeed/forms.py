f31eb5ade5bdbcf1863e2a23d17d014c4de46364
AmeliaBeep <ameliafjjames@gmail.com>
2025-11-05 18:47:16 +0000

    Add labels for mainfeed forms

--- mainfeed/forms.py
+++ mainfeed/forms.py
@@ -12,12 +12,25 @@ class PostForm(forms.ModelForm):
         options={"folder": "foodfeed/", "crop": "limit", "width": 600, "height": 600,}
         )
     
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['image'].label = "Image to share"
+        self.fields['text'].label = "Post text"
+    
 class PostTextForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text',)
+
+    def __init__(self, *args, **kwargs):
+        super(PostTextForm, self).__init__(*args, **kwargs)
+        self.fields['text'].label = "Post text"
     
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['body'].label = "Comment text"
