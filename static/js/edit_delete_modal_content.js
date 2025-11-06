fd13e45708c676da45100e18b8fee58fcc9ee54e
AmeliaBeep <ameliafjjames@gmail.com>
2025-11-06 20:11:06 +0000

    Add docstrings to script files and tweak contents

--- static/js/edit_delete_modal_content.js
+++ static/js/edit_delete_modal_content.js
@@ -1,3 +1,8 @@
+/**
+ * Adds the ability to trigger the delete confirmation modal and 
+ * sets its contents depending on whether it was triggered by a 
+ * post or comment.
+ */
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deletePostButtons = document.getElementsByClassName("delete-post-button");
 const deleteCommentButtons = document.getElementsByClassName("delete-comment-button");
@@ -19,9 +24,16 @@ for (let button of deleteCommentButtons) {
   });
 }
 
+/**
+ * Updates the delete modal content depending on whether it was triggered
+ * from a post or comment.
+ *
+ * @param {Event} e - The event object from the button click.
+ * @param {string} contentType - The type of content being deleted.
+ */
 function updateModal(e, contentType) {
-  let content_id = `${contentType}_id`
-  let contentId = e.target.getAttribute(content_id);
+  let contentIdKey = `${contentType}_id`
+  let contentId = e.target.getAttribute(contentIdKey);
   deleteModalLabel.innerText = `Delete ${contentType}?`
   deleteModalBody.innerText = `Are you sure you want to delete your ${contentType}? This action cannot be undone.`
   deleteConfirm.href = `/delete-${contentType}/${contentId}`;
--- staticfiles/js/copy_to_clipboard.js
+++ staticfiles/js/copy_to_clipboard.js
@@ -1,14 +1,22 @@
+/**
+ * Adds the ability to copy a URL to the content by 
+ * clicking on buttons with class "copy-link".
+ * 
+ * An event listener is added to these buttons which copies 
+ * the link to the clipboard and then gives a pop-up as 
+ * feedback.
+ */
 const copyLinkItems = document.querySelectorAll('.copy-link');
 
 for (let item of copyLinkItems) {
-    let link = item.getAttribute('href');
-    item.addEventListener("click", (e) => {
-        e.preventDefault();
-        let textToCopy = window.location.href;
-        textToCopy += link;
-        navigator.clipboard.writeText(textToCopy)
-        .then(() => {
-        console.log('Link copied to clipboard!');
-        })
+  let link = item.getAttribute('href');
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    let textToCopy = window.location.origin;
+    textToCopy += link;
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        alert('Link copied to clipboard!');
+      })
   });
 }
